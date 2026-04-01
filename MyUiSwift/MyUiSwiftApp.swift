c57cd12d8cdb2a4065a67893874c0b6467dcf885
Aptiway Tech <chandra.bhattiprolu@aptiway.com>
2026-04-01 15:15:17 +0530

    Initial Commit

---
+++ MyUiSwift/MyUiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyUiSwiftApp.swift
+//  MyUiSwift
+//
+//  Created by Prabhakar Bunga on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyUiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
